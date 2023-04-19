ad44f2b79a60bb2bddfa38290dd10ef15031a45f
Timothé <huarttim@gmail.com>
2023-04-19 14:17:49 +0200

    firstcommit

--- components/Home.js
+++ components/Home.js
@@ -46,7 +46,7 @@ function Home() {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:3000/movies`)
+    fetch(`https://mymoviz-backend-jzzvu81o0-huartt.vercel.app/movies`)
       .then((response) => response.json())
       .then((data) => {
         setMovie(data.movies.results);
